e034cfd8e68a103dd8cac8ec43b256cd4564541f
mistert69 <karloust@yahoo.com>
2020-01-12 18:48:49 +0100

    PWA commit

---
+++ precache-manifest.cf0311f6f8c0a52aa6284162cc3a55ad.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "893102e1869adc5bce0a94feb6f74249",
+    "url": "vendor/webcomponents-bundle.js"
+  },
+  {
+    "revision": "826ad5377a7c05b8d195",
+    "url": "main.css"
+  },
+  {
+    "revision": "da48dd1cd76dee364cc403d607193f16",
+    "url": "vendor/bundles/webcomponents-sd.js"
+  },
+  {
+    "revision": "8391957aaff84ed2243736878b5b1108",
+    "url": "vendor/custom-elements-es5-adapter.js"
+  },
+  {
+    "revision": "4d11214a32273e9537b2b7f085807ebd",
+    "url": "manifest.webmanifest"
+  },
+  {
+    "revision": "d42dd0732b85d5efa9e1660a1747b05c",
+    "url": "vendor/webcomponents-loader.js"
+  },
+  {
+    "revision": "0f9a5cf89f1d4757a6a4945a329e1fc0",
+    "url": "vendor/bundles/webcomponents-ce.js"
+  },
+  {
+    "revision": "449e154f5a93678178caf6d1d5857d65",
+    "url": "vendor/bundles/webcomponents-sd-ce-pf.js"
+  },
+  {
+    "revision": "abb358dc9afad389c31bee5903095e06",
+    "url": "vendor/bundles/webcomponents-sd-ce.js"
+  },
+  {
+    "revision": "6f7c91d4215741fbb4df483325662348",
+    "url": "index.html"
+  },
+  {
+    "revision": "23fc57b3e5ca7fcce96bb3d8de543819",
+    "url": "img/icons/icon-512x512.png"
+  },
+  {
+    "revision": "91a9501d74924f5da51f71dceffe6db5",
+    "url": "img/icons/icon-192x192.png"
+  },
+  {
+    "revision": "104b3fda1e992ee77cf88e2ba8542a4a",
+    "url": "img/icons/icon-144x144.png"
+  },
+  {
+    "revision": "9b5e91ababe48f96596f6b3494d2fa3e",
+    "url": "img/icons/favicon.png"
+  },
+  {
+    "revision": "826ad5377a7c05b8d195",
+    "url": "bundle.js"
+  }
+];
---
+++ sw.js
@@ -0,0 +1,5 @@
+importScripts("precache-manifest.cf0311f6f8c0a52aa6284162cc3a55ad.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+if ('workbox' in self) {
+    workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+  }
