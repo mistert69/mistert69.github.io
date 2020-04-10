b5fd8714edfc0e540554286253f044212fa4fec3
mistert69 <karloust@yahoo.com>
2020-04-10 09:00:45 +0200

    new interface

---
+++ precache-manifest.30f574a9ff7b7db774f3662e91129574.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f237fb8fc3db7c48225",
+    "url": "main.css"
+  },
+  {
+    "revision": "4f237fb8fc3db7c48225",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "57655846c4a9c62aebe7c3a6af9a8991",
+    "url": "index.html"
+  },
+  {
+    "revision": "3744695a13703069feec5d4909795fdd",
+    "url": "styles.css"
+  },
+  {
+    "revision": "8391957aaff84ed2243736878b5b1108",
+    "url": "vendor/custom-elements-es5-adapter.js"
+  },
+  {
+    "revision": "4d11214a32273e9537b2b7f085807ebd",
+    "url": "manifest.webmanifest"
+  },
+  {
+    "revision": "0f9a5cf89f1d4757a6a4945a329e1fc0",
+    "url": "vendor/bundles/webcomponents-ce.js"
+  },
+  {
+    "revision": "d42dd0732b85d5efa9e1660a1747b05c",
+    "url": "vendor/webcomponents-loader.js"
+  },
+  {
+    "revision": "893102e1869adc5bce0a94feb6f74249",
+    "url": "vendor/webcomponents-bundle.js"
+  },
+  {
+    "revision": "abb358dc9afad389c31bee5903095e06",
+    "url": "vendor/bundles/webcomponents-sd-ce.js"
+  },
+  {
+    "revision": "449e154f5a93678178caf6d1d5857d65",
+    "url": "vendor/bundles/webcomponents-sd-ce-pf.js"
+  },
+  {
+    "revision": "da48dd1cd76dee364cc403d607193f16",
+    "url": "vendor/bundles/webcomponents-sd.js"
+  },
+  {
+    "revision": "163776c885b8cfa4402510570fc14e02",
+    "url": "img/baseball.jpg"
+  },
+  {
+    "revision": "265a4a61a2d0cb79fe2f716e12029a29",
+    "url": "img/icons/icon-512x512.png"
+  },
+  {
+    "revision": "22cf434c35485a385ee356882fbbacb3",
+    "url": "img/icons/icon-192x192.png"
+  },
+  {
+    "revision": "028be18872917eee9d192f7ef2c7d354",
+    "url": "img/icons/favicon.png"
+  },
+  {
+    "revision": "0421b4b74aa24572c428a97bc19880eb",
+    "url": "img/icons/icon-144x144.png"
+  }
+];
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.cf0311f6f8c0a52aa6284162cc3a55ad.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.30f574a9ff7b7db774f3662e91129574.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 if ('workbox' in self) {
     workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
